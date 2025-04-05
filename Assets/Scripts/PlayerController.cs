a2c35af5c82064e384b00d4096b56b6478168e55
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-04-05 23:45:24 +0300

    Finish Commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -5,6 +6,9 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public bool hasPowerUp;
+    public float powerStrength = 15.0f;
+    public GameObject PowerIndicator;
 
     private void Start()
     {
@@ -15,5 +19,32 @@ public class PlayerController : MonoBehaviour
     {
         float verticalInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * verticalInput);
+        PowerIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            PowerIndicator.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerCountDownRoutine());
+        }
+    }
+    IEnumerator PowerCountDownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        PowerIndicator.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Debug.Log("Collied With" + collision.gameObject.name + "with powerup set to" + hasPowerUp);
+            Rigidbody enemyRB = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            enemyRB.AddForce(awayFromPlayer * powerStrength, ForceMode.Impulse);
+        }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,10 +3,23 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefaps;
+    public GameObject PowerUpPrefaps;
     private float spawnRange = 9;
+    public int enemyCount;
+    public int enemyVawe;
     private void Start()
     {
-       Instantiate(enemyPrefaps, GenerateSpawnPosition() , enemyPrefaps.transform.rotation);
+        Instantiate(PowerUpPrefaps, GenerateSpawnPosition(), PowerUpPrefaps.transform.rotation);
+    }
+    private void Update()
+    {
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+        if(enemyCount == 0)
+        {
+            enemyVawe++;
+            SpawnEnemyWave(enemyVawe);
+            Instantiate(PowerUpPrefaps, GenerateSpawnPosition(), PowerUpPrefaps.transform.rotation);
+        } 
     }
     private Vector3 GenerateSpawnPosition()
     {
@@ -14,5 +27,12 @@ public class SpawnManager : MonoBehaviour
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
+    }
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefaps, GenerateSpawnPosition(), enemyPrefaps.transform.rotation);
+        }
     }
 }
